0d731d05cfad1013b46800b9d4ed56d5b93d7466
pedcobram <pedcobram@alum.us.es>
2021-10-03 22:16:22 +0200

    Section 10: Services & Dependency Injection

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,21 +8,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  @Output()
-  recipeWasSelected = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe('Test Recipe 1', 'This is a test description for a recipe 1', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('Test Recipe 2', 'This is a test description for a recipe 2', 'https://barradeideas.com/wp-content/uploads/2019/09/fast-food.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
-
 }
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,38 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Recipe } from "./recipe.model";
+
+@Injectable()
+export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe('Ribs with greens on the side', 
+        'This is a test description for a recipe 1', 
+        'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg', 
+        [
+            new Ingredient('Ribs', 1),
+            new Ingredient('Tomatoes', 2),
+            new Ingredient('Green Peppers', 2)
+        ]),
+        new Recipe('Hamburguer and fries', 
+        'This is a test description for a recipe 2', 
+        'https://barradeideas.com/wp-content/uploads/2019/09/fast-food.jpg',
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('Buns', 2),
+            new Ingredient('Fries', 20)
+        ])
+    ];
+
+    constructor(private slService: ShoppingListService) { }
+
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
+    }
+}
